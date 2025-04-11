592424e299c588a234bf708de494737dd86b7f89
Jordan Youner <jordanyouner@brandeis.edu>
2025-04-11 15:12:20 -0400

    added more args, including an eval only mode

--- train_slm.py
+++ train_slm.py
@@ -5,7 +5,9 @@ import torch
 from preprocess import load_data, compute_metrics
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument("-model_name")
 parser.add_argument("-lang_dir")
+parser.add_argument("-eval_only")
 parser.add_argument("-use_lora")
 parser.add_argument("-use_space")
 parser.add_argument("-out_dir")
@@ -20,9 +22,9 @@ def tokenize_data(data):
     return tokenizer(data["string"], padding="max_length", truncation=True)
 
 
-def train_eval_bert(use_lora: bool, lora_rank: int, use_rs: bool, data, output_dir: str, out_file:str = "results.txt"):
+def train_eval_bert(model_name: str, eval_only: bool, use_lora: bool, lora_rank: int, use_rs: bool, data, output_dir: str, out_file:str = "results.txt"):
     device = "cuda"
-    model = CanineForSequenceClassification.from_pretrained("google/canine-c").to(device)
+    model = CanineForSequenceClassification.from_pretrained(model_name).to(device)
     config = LoraConfig(
         r=lora_rank,
         lora_alpha=32,
@@ -55,7 +57,9 @@ def train_eval_bert(use_lora: bool, lora_rank: int, use_rs: bool, data, output_d
             eval_dataset=data["dev"],
             compute_metrics=compute_metrics
         )
-    trainer.train()
+    # training and eval only modes
+    if not eval_only:
+        trainer.train()
     # iterate through all test sets in the constant
     for test_set in TEST_EVAL_ORDER:
         evaluated = trainer.evaluate(data[test_set])
@@ -67,14 +71,16 @@ def train_eval_bert(use_lora: bool, lora_rank: int, use_rs: bool, data, output_d
 if __name__ == "__main__":
     # get args
     args = parser.parse_args()
+    model_name = args.model_name
+    eval_only = args.eval_only
     directory = args.lang_dir
     if_lora = args.use_lora
     out_dir = args.out_dir
     results_file = args.results_file
     use_space = args.use_space
-    tokenizer = CanineTokenizer.from_pretrained("google/canine-c")
+    tokenizer = CanineTokenizer.from_pretrained(model_name)
     accuracy_metric = evaluate.load("accuracy")
     f1_metric = evaluate.load("f1")
     dataset = load_data(directory, use_spaces=use_space)
     tokenized_dataset = dataset.map(tokenize_data, batched=True)
-    train_eval_bert(use_lora=if_lora, lora_rank=32, use_rs=False, data=tokenized_dataset, output_dir="rank32_ZP", out_file=results_file)
+    train_eval_bert(model_name=model_name, eval_only=eval_only, use_lora=if_lora, lora_rank=32, use_rs=False, data=tokenized_dataset, output_dir="rank32_ZP", out_file=results_file)
